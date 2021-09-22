3fba1db34238ebd56c7fadb5ce21251dec13c6b0
Luan Grabher <luanggcontato@gmail.com>
2021-09-22 14:39:04 -0300

    add number of used robots in return

--- src/robot/moresco/countusedrobots/Controller.java
+++ src/robot/moresco/countusedrobots/Controller.java
@@ -4,16 +4,19 @@ import Entity.Executavel;
 import Entity.Warning;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 import robot.moresco.countusedrobots.Model.ServerFiles_Model;
 import robot.moresco.countusedrobots.Model.ZAC_Model;
+import static robot.moresco.countusedrobots.RobotMorescoCountUsedRobots.month;
 
 public class Controller {
 
     public static List<String> ignoreUsers = new ArrayList<String>(Arrays.asList("ti01"));
     public static List<String> ignoreTasks = new ArrayList<String>(Arrays.asList("InsertOutlookSignature"));
+    public Integer ignoredTasks = 0;
 
     // Tarefa --> Usuario --> Contagem
     public static Map<String, Map<String, Integer>> uses = new TreeMap<>();
@@ -37,6 +40,8 @@ public class Controller {
             table.append("</th>");
 
             uses.forEach((task, users) -> {
+                Boolean[] ignored = new Boolean[]{false};
+                
                 users.forEach((user, count) -> {
                     if (!ignoreUsers.contains(user) && !ignoreTasks.contains(task)) {
                         table.append("<tr>");
@@ -44,13 +49,21 @@ public class Controller {
                         table.append("<td>").append(user).append("</td>");
                         table.append("<td>").append(count).append("</td>");
                         table.append("</tr>");
+                    }else{
+                        ignored[0] = true;
                     }
                 });
+                
+                if(ignored[0]){
+                    ignoredTasks++;
+                }
             });
 
             table.append("</table>");
+            
+            System.out.println(uses);
 
-            throw new Warning(table.toString());
+            throw new Warning("Total de Robôs e Programas usados no mês " + (month + 1) + ": " + (uses.size() - ignoredTasks) + "<br><br>" + table.toString());
         }
 
     }
